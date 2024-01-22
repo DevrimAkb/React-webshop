4eab29bd4e50abffc1c4fde91d1fd7ad2288c6e0
Devrim Akbulut <Devrimakb1@gmail.com>
2024-01-22 16:22:35 +0100

    contact form done, no validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export const Form = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const res = await axios.post('https://js2-ecommerce-api.vercel.app/api/messages', {
+                name,
+                email,
+                message
+            })
+            if(res.status === 200) {
+                console.log('Message sent successfully')
+                setName('')
+                setEmail('')
+                setMessage('')
+            }
+            else {
+                console.log('Something went wrong')
+            }
+        } 
+        catch(error) {
+            console.log('Error submitting the form', error)
+        }
+    }
+
   return (
-    <form className="contact-form">
+    <form onSubmit={handleSubmit} className="contact-form">
         <div className="form-group">
-            <label htmlFor="firstName" className='form-label'>First name</label>
-            <input type="text" id='firstName' className='form-control' />
-            <p className="invalid-input">Error message</p>
-        </div>
-        <div className="form-group">
-            <label htmlFor="lastName" className='form-label'>Last name</label>
-            <input type="text" id='lastName' className='form-control' />
+            <label htmlFor="name" className='form-label'>Name</label>
+            <input type="text" id='name' className='form-control' value={name} onChange={e => setName(e.target.value)} />
             <p className="invalid-input">Error message</p>
         </div>
         <div className="form-group">
             <label htmlFor="email" className='form-label'>Email</label>
-            <input type="email" id='email' className='form-control' />
+            <input type="email" id='email' className='form-control' value={email} onChange={e => setEmail(e.target.value)} />
             <p className="invalid-input">Error message</p>
         </div>
         <div className="form-group">
             <label htmlFor="message" className='form-label'>Your message</label>
-            <textarea name="message" id="message" cols="30" rows="10"></textarea>
+            <textarea name="message" id="message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
             <p className="invalid-input">Error message</p>
         </div>
-        <button className="btn submit-btn">Send</button>
+        <button type="submit" className="btn contact-send-btn">Send</button>
     </form>
   )
 }
